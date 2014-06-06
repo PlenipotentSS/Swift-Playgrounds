0f035203e622e8ec8bc491e5b66dd980688ba9fb
PlenipotentSS <stevensonhoyt@icloud.com>
2014-06-06 14:22:10 -0700

    update with advanced swift notes

--- AdvancedPlayground.playground/section-1.swift
+++ AdvancedPlayground.playground/section-1.swift
@@ -19,6 +19,12 @@ for (key, _) in dictionary {
 
 var color = UIColor.greenColor()
 
+extension UIColor {
+    func rgba() ->() {  //how to return tuple?
+        return (1,2,3,4)
+    }
+}
+
 var red, blue: Int
 (red, _, blue, _) = color.rgba()
 
@@ -176,6 +182,264 @@ onePlace[.North]        //shortcut using connections
 /*
 
     SWIFT GENERICS!!!!
+    Making functions and methods behave on a generic type!!
+*/
+func peek(value: String) {
+    println("[peek] \(value)")
+}
+func peek(value: Int) {
+    println("[peek] \(value)")
+}
+func peek(value: Float) {
+    println("[peek] \(value)")
+}
+
+// why not use a generic type?
+// even grabs more!
+func peek<T>(value: T) -> T {
+    println("[peek] \(value)")
+}
+
+extension String {
+    var sansEmoji: String {
+        get {
+            //remove any emojis!
+        }
+    }
+}
+
+println( peek().capitalizedString().sansEmoji )
+
+
+/*
+
+    DYNAMIC POLYMORPHISM
+    -> Type information is thrown away
+
+    Generics Conserve type information!
+    -> Great for type safety
+    -> Great for Performance
 */
 
+
+// work to swap out two objects, regardless of type
+// but just has to be the same type!!
+func swap<T>(inout x: T, inout y: T) {
+    let tmp = x
+    x = y
+    y = tmp
+}
+
+
+func indexOfString(sought: String, inArray array: String[]) -> Int? {
+    for i in 0..array.count {
+        if array[i] == sought {
+            return i
+        }
+    }
+    return nil
+}
+
+//-> To Generics:
+protocol Equatable {
+    func == (lhs: Self, rhs: Self) -> Bool
+}
+
+// using equatable to ensure there is a way to compare two objects
+func indexOf<T : Equatable>(sought: T, inArray array: T[]) -> Int? {
+    for i in 0..array.count {
+        if ( array[i] == sought) {
+            return i
+        }
+    }
+    return nil
+}
+
+//protocol can conform to almost anything
+struct Temperature: Equatable {
+    let value: Int = 0
+}
+
+//specific to operator requirements (must be at file scope level)
+func == (lhs: Temperature, rhs: Temperature) -> Bool {
+    return lhs.value == rhs.value
+}
+
+//don't need to write not equal! because:
+func != <T : Equatable> (lhs: Temperature, rhs: Temperature) -> Bool {
+    return !(lhs == rhs)
+}
+
+
+/*
+
+
+    Computing the golden mean!
+    (ratios of the consecutive fibonacci numbers)
+
+*/
+func fibonacci(n: Int) -> Double {
+    return n < 2 ? Double(n) : fibonacci(n-1) + fibonacci(n-2)
+}
+
+let phi = fibonacci(45) / fibonacci(44) //takes 42 seconds
+// doing this because there is repetitious calls (fibonacci(n-1) may be the same as fibonacci(n-2)
+// now lets use memoization!
+
+//lets improve it:
+
+var fibonacciMemo = Dictionary<Int, Double>()
+
+func fibonacciGood(n: Int) -> Double {
+    if let result = fibonacciMemo[n] {
+        return result
+    }
+    let result = n < 2 ? Double(n) : fibonacciGood(n-1) + fibonacciGood(n+2)
+    fibonacciMemo[n] = result
+    return result
+}
+
+let phi_2 = fibonacciGood(45) / fibonacciGood(44)   //takes .1 seconds
+
+
+
+//How To Memoize any function!!!
+func memoize<T : Hashable, U>( body: (T)->U ) -> (T)->U {
+    var memo = Dictionary<T, U>()   //T must be hashable to become key!
+    return {
+        x in
+        if let q = memo[x] { return q }
+        let r = body(x)
+        memo[x] = r
+        return r
+    }
+}
+
+//make any recursive call into a double initializion dance:
+//take 1:
+var factorial: (Int)->Int = { $0 }  //identity function!
+factorial = memoize { x in x == 0 ? 1 : x * factorial(x-1) }
+
+
+//take 2:
+func memoize<T : Hashable, U>( body: ((T)->U, T)->U ) -> (T)->U {
+    var memo = Dictionary<T, U>()   //T must be hashable to become key!
+    var result: ((T)->U)!
+    result = {
+        x in
+        if let q = memo[x] { return q }
+        let r = body(result, x)
+        memo[x] = r
+        return r
+    }
+    return result
+}
+
+/// WOW: add factorial back into the body, called factorial (new closure)
+let factorialTake2 = memoize { factorial, x in x == 0 ? 1: x * factorial(x - 1) }
+
+/*
+The Power:
+
+Concision:
+let fibonacci =
+
+Closure Suntax!
+let fibonacci = memoize { ... }
+
+Generics!!
+func memoize<T : Hashable, U>( body: ((T)->U, T)->U) -> (T)->U
+*/
+
+
+/*
+    Generic Types:
+    
+    Seen them before:
+    -> Arrays, Dictionaries
+    -> Optionals (generic enums)
+
+    Now: Struct:
+*/
+
+struct StringStack {
+    mutating func push(x: String) {
+        items += x
+    }
+    mutating func pop() -> String {
+        return items.removeLast()
+    }
+    var items: String[]
+}
+
+//can be as easy as replacing the type arguments
+struct Stack<T>{
+    mutating func push(x: T) {
+        items += x
+    }
+    mutating func pop() -> T {
+        return items.removeLast()
+    }
+    var items: T[]
+    
+     init() {
+        items
+    }
+}
+
+var intStack = Stack<Int>()
+intStack.push(42)
+//var windowStack = Stack<NSWindow>()
+
+//problem!!:
+
+func peekStack(s: Stack<Int>) {
+    for x in s { println(x) }   //issue because stack does not conform to sequence protocol
+}
+
+//under the hood, it calls s.generaor()
+// followed by next()
+// gotta make a Generator Protocol!
+
+// Swift's base protocol:
+protocol Generator {
+    typealias Element
+    mutating func next() -> Element?
+}
+
+// now we rewrite our stackgenerator as a struct:
+
+struct StackGenerator<T>: Generator {
+//    typealias Element = T // not necessary because T? is assumed on next line
+    mutating func next() -> T? {
+        if items.isEmpty { return nil }
+        let ret = items[0]
+        items = items[1..items.count]
+        return ret
+    }
+    
+    var items: Slice<T> //slices the array in O(1)
+}
+
+//now the sequence:
+//swift's base protocol:
+protocol Sequeuence {
+    typealias GeneratorType : Generator
+    func generate() -> GeneratorType
+}
+
+//now our addon extension:
+extension Stack : Sequence {
+    func generate() -> StackGenerator<T> {
+        return StackGenerator( items[0..items.count])
+    }
+}
+
+//need to now conform Stack<T> to conform to Sequence!
+
+
+/*
+
+    HIGH LEVEL ABASTRACTION
+*/
 
